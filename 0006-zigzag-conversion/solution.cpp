414d0eddf4808ad56a13860258192093b986952f
buffer <iamrealhayden@gmail.com>
2025-06-03 06:52:41 +0000

    chore: sync automated leetcode submissions with Runtime - 7 ms (45.25%), Memory - 14.2 MB (39.10%)

--- 0006-zigzag-conversion/solution.cpp
+++ 0006-zigzag-conversion/solution.cpp
@@ -1,19 +1,34 @@
+
+
 class Solution {
 public:
+
     string convert(string s, int numRows) {
-        if (numRows == 1) return s;
-        char a[10000];
-        std::size_t len = s.length();
-        int index =0;
-        for (int i = 0; i < numRows; i++) {
-            for (int j = i; j < len; j +=2*(numRows-1)) {
-                a[index++] = s[j];
-                if (i > 0 && i<numRows-1 && j+2*(numRows-1)-2*i<len) {
-                    a[index++] = s[j + 2*(numRows - 1)-2*i];
-                }
-            }
-        }
-        a[index]='\0';
-        return a;
+    
+    if(numRows <= 1) return s;
+
+    vector<string>v(numRows, ""); 
+
+    int j = 0, dir = -1;
+
+    for(int i = 0; i < s.length(); i++)
+    {
+
+        if(j == numRows - 1 || j == 0) dir *= (-1); 
+		 
+        v[j] += s[i];
+
+        if(dir == 1) j++;
+
+        else j--;
+    }
+
+    string res;
+
+    for(auto &it : v) res += it; 
+
+    return res;
+
     }
 };
+
