1c29498c875d10e19a486c470df4c10d3b3e9fe2
buffer <iamrealhayden@gmail.com>
2025-06-03 07:00:15 +0000

    chore: sync automated leetcode submissions with Runtime - 0 ms (100.00%), Memory - 41.3 MB (89.59%)

--- 0007-reverse-integer/solution.java
+++ 0007-reverse-integer/solution.java
@@ -1,22 +1,16 @@
 class Solution {
     public int reverse(int x) {
-        int result = 0;
-        int pop = 0;
+        return solution(x, 0);
+    }
 
-        while( x != 0){
-            pop = x % 10;
-            x = x / 10;
+    public int solution(int x, int rev) {
+        if (x == 0) return rev;
+        if (rev > Integer.MAX_VALUE / 10 || rev < Integer.MIN_VALUE / 10) return 0;
 
-            if(result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE / 10 && pop > 7)){
-            return 0;
-        }
+        int rem = x % 10;
+        x = x / 10;
+        rev = rev * 10 + rem;
 
-        else if(result < Integer.MIN_VALUE / 10 || (result == Integer.MIN_VALUE / 10 && pop < -8)){
-            return 0;
-        }
-        result = result * 10 + pop;
-        }
-
-        return result;
+        return solution(x, rev);
     }
 }
