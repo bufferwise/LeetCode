a3b4b6a3f7a0c291b5afe9a1a66176d29ed94bcf
buffer <iamrealhayden@gmail.com>
2025-06-03 06:56:39 +0000

    chore: sync automated leetcode submissions with Runtime - 2 ms (99.50%), Memory - 44.9 MB (96.98%)

--- 0006-zigzag-conversion/solution.java
+++ 0006-zigzag-conversion/solution.java
@@ -1,15 +1,36 @@
 class Solution {
-    public String convert(String s, int numRows) {
-        if (numRows == 1) return s;
-        StringBuilder a = new StringBuilder();
-        for (int i = 0; i < numRows; i++) {
-            for (int j = i; j < s.length(); j += (2 * (numRows - 1))) {
-                a.append(s.charAt(j));
-                if (i > 0 && i < numRows - 1 && j + (2 * (numRows - 1)) - (2 * i) < s.length()) {
-                    a.append(s.charAt(j + (2 * (numRows - 1)) - (2 * i)));
+    public static String convert(String arr, int rows) {
+        int std = 2*rows - 2;
+        int len = arr.length();
+        char[] ans = new char[len];
+        int point = 0;
+        if(rows == 1 || rows > len) return arr;
+        for(int i = 0 ; i < rows ; i++) {
+            int j = i;
+            ans[point++] = arr.charAt(j);
+            while(true)
+            {
+                int k = j;
+                if(j + 2*(rows-i-1) >= len) break;
+                if(i != rows-1)
+                {
+                    j = j + 2*(rows-i-1);
+                    ans[point++] = arr.charAt(j);
+                }
+                if(j + 2*i >= len) break;
+                if(i!=0)
+                {
+                    j = j + 2*(i);
+                    ans[point++] = arr.charAt(j);
+                }
+                if(k == j)
+                {
+                    break;
                 }
             }
         }
-        return a.toString();
+
+        String answer = new String(ans);
+        return answer;
     }
 }
