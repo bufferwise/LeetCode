675b640ebd6d2795aff002730c2e1eed26f2376d
buffer <iamrealhayden@gmail.com>
2025-06-04 05:15:50 +0000

    chore: sync automated leetcode submissions with Runtime - 69 ms (55.60%), Memory - 169.2 MB (22.63%)

--- 3683-find-the-lexicographically-largest-string-from-the-box-i/solution.cpp
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/solution.cpp
@@ -1,40 +1,34 @@
-class Solution {
- public:
-  string answerString(string word, int numFriends) {
-    if (numFriends == 1)
-      return word;
-    const string s = lastSubstring(word);
-    const size_t sz = word.length() - numFriends + 1;
-    return s.substr(0, min(s.length(), sz));
-  }
+class Solution 
+{
+public:
+    string answerString(string word, int numFriends) 
+    {
+        // Step 1: Edge case
+        if (numFriends == 1) 
+	{
+		return word;
+	}
 
- private:
-  // Same as 1163. Last Substring in Lexicographical Order
-  string lastSubstring(string s) {
-    int i = 0;
-    int j = 1;
-    int k = 0;
+        int n = word.size();
+    
+        // Step 2: Compute max part length
+        int maxLen = n - numFriends + 1;
+        string res = "";
 
-    while (j + k < s.length())
-      if (s[i + k] == s[j + k]) {
-        ++k;
-      } else if (s[i + k] > s[j + k]) {
-        // Skip s[j..j + k) and advance to s[j + k + 1] to find a possible
-        // lexicographically larger substring since s[i..i + k) == s[j..j + k)
-        // and s[i + k] > s[j + k).
-        j = j + k + 1;
-        k = 0;
-      } else {
-        // Skip s[i..i + k) and advance to s[i + k + 1] or s[j] to find a
-        // possible lexicographically larger substring since
-        // s[i..i + k) == s[j..j + k) and s[i + k] < s[j + k).
-        // Note that it's unnecessary to explore s[i + k + 1..j) if
-        // i + k + 1 < j since they are already explored by j.
-        i = max(i + k + 1, j);
-        j = i + 1;
-        k = 0;
-      }
+        // Step 3: Iterate over all valid substrings
+        for (int i = 0; i < n; ++i) 
+        {
+            int len = min(n - i, maxLen);
+            string substr = word.substr(i, len);
 
-    return s.substr(i);
-  }
+            // Step 4: Compare and update result
+            if (substr > res) 
+            {
+                res = substr;
+            }
+        }
+
+        // Step 5: Return the result
+        return res;
+    }
 };
