c298eea14aeb9ce6d39d4aefb07087ea076c2dc2
buffer <iamrealhayden@gmail.com>
2025-06-18 10:48:06 +0000

    chore: sync automated leetcode submissions with Runtime - 16 ms (96.97%), Memory - 45.8 MB (100.00%)

--- 3777-maximum-product-of-subsequences-with-an-alternating-sum-equal-to-k/solution.java
+++ 3777-maximum-product-of-subsequences-with-an-alternating-sum-equal-to-k/solution.java
@@ -1,64 +1,106 @@
-public class Solution {
-    public int maxProduct(int[] a, int k, int l) {
-        int I = Integer.MAX_VALUE;
-        Map<String, Set<Integer>> d = new HashMap<>();
+class Solution {
+	private static final int MAX_NUM = 12;
 
-        for (int xd : a) {
-            Map<String, Set<Integer>> nd = new HashMap<>();
+	private static class SumProduct {
+		final int altSum;
+		final int product;
 
-            for (Map.Entry<String, Set<Integer>> e : d.entrySet()) {
-                nd.put(e.getKey(), new HashSet<>(e.getValue()));
-            }
+		SumProduct(int altSum, int product) {
+			this.altSum = altSum;
+			this.product = product;
+		}
 
-            if (xd == 0) {
-                String key = "1#0";
-                nd.computeIfAbsent(key, z -> new HashSet<>()).add(0);
-            } else {
-                if (xd <= l) {
-                    String key = "1#" + xd;
-                    nd.computeIfAbsent(key, z -> new HashSet<>()).add(xd);
-                } else {
-                    String key = "1#" + I;
-                    nd.computeIfAbsent(key, z -> new HashSet<>()).add(xd);
-                }
-            }
+		@Override
+		public int hashCode() {
+			return altSum * 6827 + product;
+		}
 
-            for (Map.Entry<String, Set<Integer>> e : d.entrySet()) {
-                String[] p = e.getKey().split("#");
-                int pr = Integer.parseInt(p[0]);
-                int pd = Integer.parseInt(p[1]);
-                for (int s : e.getValue()) {
-                    int np = 1 - pr;
-                    int na = pr == 0 ? s + xd : s - xd;
-                    int npd;
+		@Override
+		public boolean equals(Object obj) {
+			SumProduct other = (SumProduct) obj;
+			return altSum == other.altSum && product == other.product;
+		}
+	}
 
-                    if (xd == 0) {
-                        npd = 0;
-                    } else {
-                        if (pd == I || (long) pd * xd > l) {
-                            npd = I;
-                        } else {
-                            npd = pd * xd;
-                        }
-                    }
+	private static int maxInSet(Set<SumProduct> set, int targetSum) {
+		int maxProd = 0;
+		for (SumProduct sp : set)
+			if (sp.altSum == targetSum && sp.product > maxProd)
+				maxProd = sp.product;
+		return maxProd;
+	}
 
-                    String key = np + "#" + npd;
-                    nd.computeIfAbsent(key, z -> new HashSet<>()).add(na);
-                }
-            }
+	private static int maxNonZeroProduct(int[] nums, int targetSum, int limit) {
+		Set<SumProduct> oddSet = new LinkedHashSet<>();
+		Set<SumProduct> evenSet = new LinkedHashSet<>();
+		for (int x : nums) {
+			if (x == 0)
+				continue;
+			SumProduct firstNewEven = null;
+			for (SumProduct sp : oddSet) {
+				int p = sp.product * x;
+				if (p <= limit) {
+					SumProduct spx = new SumProduct(sp.altSum - x, p);
+					if (evenSet.add(spx) && firstNewEven == null)
+						firstNewEven = spx;
+				}
+			}
+			for (SumProduct sp : evenSet) {
+				if (sp == firstNewEven)
+					break;
+				int p = sp.product * x;
+				if (p <= limit)
+					oddSet.add(new SumProduct(sp.altSum + x, p));
+			}
+			if (x <= limit)
+				oddSet.add(new SumProduct(x, x));
+		}
+		return Math.max(maxInSet(oddSet, targetSum), maxInSet(evenSet, targetSum));
+	}
 
-            d = nd;
-        }
+	private static void mixedAdd(Set<Integer> oddSums, Set<Integer> evenSums, int a) {
+		Integer firstNewEven = null;
+		for (Integer x : oddSums) {
+			Integer s = x - a;
+			if (evenSums.add(s) && firstNewEven == null)
+				firstNewEven = s;
+		}
+		for (Integer x : evenSums) {
+			if (x == firstNewEven)
+				break;
+			oddSums.add(x + a);
+		}
+	}
 
-        int rs = -1;
-        for (Map.Entry<String, Set<Integer>> e : d.entrySet()) {
-            String[] p = e.getKey().split("#");
-            int pd = Integer.parseInt(p[1]);
-            if (pd != I && e.getValue().contains(k)) {
-                rs = Math.max(rs, pd);
-            }
-        }
+	private static boolean zeroProductExists(int[] nums, Integer targetSum) {
+		// With zero product
+		Set<Integer> oddSumsZ = new LinkedHashSet<>();
+		Set<Integer> evenSumsZ = new LinkedHashSet<>();
+		// With positive product
+		Set<Integer> oddSumsP = new LinkedHashSet<>();
+		Set<Integer> evenSumsP = new LinkedHashSet<>();
+		for (int x : nums) {
+			if (x == 0) {
+				oddSumsZ.addAll(evenSumsZ);
+				evenSumsZ = new LinkedHashSet<>(oddSumsZ);
+				oddSumsZ.addAll(evenSumsP);
+				evenSumsZ.addAll(oddSumsP);
+				oddSumsZ.add(0);
+			} else {
+				mixedAdd(oddSumsZ, evenSumsZ, x);
+				mixedAdd(oddSumsP, evenSumsP, x);
+				oddSumsP.add(x);
+			}
+			if (oddSumsZ.contains(targetSum) || evenSumsZ.contains(targetSum))
+				return true;
+		}
+		return false;
+	}
 
-        return rs;
-    }
+	public static int maxProduct(int[] nums, int k, int limit) {
+		if ((nums.length + (k > 0 ? 1 : 0)) / 2 * MAX_NUM < Math.abs(k))
+			return -1;
+		int p = maxNonZeroProduct(nums, k, limit);
+		return p > 0 ? p : zeroProductExists(nums, k) ? 0 : -1;
+	}
 }
