247cd1a09550cd52f42c3b0c07b6a1cba228f02d
buffer <iamrealhayden@gmail.com>
2025-06-18 10:29:09 +0000

    chore: sync automated leetcode submissions Runtime - 1 ms (100.00%), Memory - 41.8 MB (98.89%)

---
+++ 1058-lexicographically-smallest-equivalent-string/solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String smallestEquivalentString(String s1, String s2, String baseStr) {
+        int[] parent = new int[26];
+        for (int i = 0; i < 26; i++) parent[i] = i;
+        
+        for (int i = 0; i < s1.length(); i++) {
+            union(parent, s1.charAt(i) - 'a', s2.charAt(i) - 'a');
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for (char c : baseStr.toCharArray()) {
+            sb.append((char)(find(parent, c - 'a') + 'a'));
+        }
+        return sb.toString();
+    }
+    
+    private int find(int[] parent, int x) {
+        if (parent[x] != x) parent[x] = find(parent, parent[x]);
+        return parent[x];
+    }
+    
+    private void union(int[] parent, int x, int y) {
+        int px = find(parent, x), py = find(parent, y);
+        if (px != py) {
+            if (px < py) parent[py] = px;
+            else parent[px] = py;
+        }
+    }
+}
