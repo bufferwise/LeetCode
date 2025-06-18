8732458add7dc0f828f1e4c52fff624000d5d6ac
buffer <iamrealhayden@gmail.com>
2025-06-18 10:49:53 +0000

    chore: sync automated leetcode submissions with Runtime - 1838 ms (49.18%), Memory - 508.4 MB (63.53%)

--- 3777-maximum-product-of-subsequences-with-an-alternating-sum-equal-to-k/solution.cpp
+++ 3777-maximum-product-of-subsequences-with-an-alternating-sum-equal-to-k/solution.cpp
@@ -1,33 +1,41 @@
-struct Range { int val, min, max, smallest_digit; };
-vector<Range> range;
-int L;
-int bestProduct = -1;
+const int MIN = -5000;
 
 class Solution {
 public:
-  static void choose(int i, unsigned long long prodSoFar, int k) {
-    if (bestProduct == L) return;
-    if (k > range[i].max || k < range[i].min) return;
-    if (prodSoFar*range[i].smallest_digit > L) return;
-    for(int j = i; j < range.size()-1; ++j) {
-      unsigned long long newProd = prodSoFar * range[j].val;
-      if (k == range[j].val && newProd <= L) bestProduct = max<int>(newProd, bestProduct);
-      if (bestProduct == -1 || (newProd > 0 && newProd <= L))
-        choose(j+1, newProd, range[j].val - k);
+    unordered_map<int, unordered_map<int, unordered_map<int, unordered_map<int, int>>>> dp;
+    int recursion(int pos, int currSum, int product, int isOdd, int k, int n, vector<int> &nums, int limit) {
+        if(pos == n) {
+            return (currSum == k && isOdd != 0 && product <= limit ? product : MIN);
+        }
+        
+        if(dp.count(pos) && dp[pos].count(currSum) && dp[pos][currSum].count(product) && dp[pos][currSum][product].count(isOdd)) {
+            return dp[pos][currSum][product][isOdd];
+        }
+            
+        int ans = recursion(pos + 1, currSum, product, isOdd, k, n, nums, limit);
+        if(isOdd == 0) {
+            ans = max(ans, recursion(pos + 1, currSum + nums[pos], nums[pos], 2, k, n, nums, limit));
+        }
+        if(isOdd == 1) {
+            ans = max(ans, recursion(pos + 1, currSum + nums[pos], min(product * nums[pos], limit + 1), 2, k, n, nums, limit));
+        }
+        if(isOdd == 2) {
+            ans = max(ans, recursion(pos + 1, currSum - nums[pos], min(product * nums[pos], limit + 1), 1, k, n, nums, limit));
+        }
+        
+        return dp[pos][currSum][product][isOdd] = ans;
     }
-  }
-  int maxProduct(const vector<int> &nums, int k, int limit) {
-    range.resize(nums.size()+1);
-    range[range.size()-2] = { nums.back(), 0, nums.back(), nums.back() };
-    for(int i = (int)nums.size()-2; i >= 0; --i) {
-      range[i].val = nums[i];
-      range[i].max = max(range[i+1].max, nums[i] - range[i+1].min);
-      range[i].min = min(range[i+1].min, nums[i] - range[i+1].max);
-      range[i].smallest_digit = min(nums[i], range[i+1].smallest_digit);
+    
+    int maxProduct(vector<int> &nums, int k, int limit) {
+        int n = nums.size();
+        
+        int sum = 0;
+        for(auto &x: nums) sum += x;
+        
+        if(k > sum || k < -sum) return -1;
+            
+        dp.clear();
+        int ans = recursion(0, 0, 0, 0, k, n, nums, limit);
+        return (ans == MIN) ? -1 : ans;
     }
-    bestProduct = -1;
-    L = limit;
-    choose(0, 1, k);
-    return bestProduct;
-  }
 };
