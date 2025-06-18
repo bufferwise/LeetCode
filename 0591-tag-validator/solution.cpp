0e65bcb44ab6010c8444915b5a51fdd62cdc196b
buffer <iamrealhayden@gmail.com>
2025-06-18 10:40:44 +0000

    chore: sync automated leetcode submissions with Runtime - 6 ms (2.75%), Memory - 11.4 MB (13.06%)

--- 0591-tag-validator/solution.cpp
+++ 0591-tag-validator/solution.cpp
@@ -1,70 +1,49 @@
 class Solution {
-private:
-    int now;
-    bool cdata(string &code)
-    {
-        constexpr char head[] = "[CDATA[" , end[] = "]]>";
-        constexpr int head_len = 7 , end_len = 3;
-        now += 2;
-        for(int i = 0 ; i < head_len ; i++)
-        {
-            if(now + i >= code.size())return 0;
-            if(code[now+i] != head[i])return 0;
-        }
-        now += head_len;
-        for(int k = 0 ; k < end_len ; now++)
-        {
-            if(now >= code.size())return 0;
-            if(code[now] == end[k])k++;
-            else k = 0;
-        }
-        now--;
-        return 1;
-    }
-    bool tag(string &code)
-    {
-        int start = now+1;
-        // read tag name
-        if(code[now++] != '<')return 0;
-        for(int i = 0 ; i < 10 ; i++)
-        {
-            if(now + i == code.size())return 0;
-            if(i && code[now+i] == '>')
-            {
-                now += i;
-                break;
-            }
-            if(code[now+i] < 'A' || code[now+i] > 'Z')return 0;
-            if(i == 9)return 0;
-        }
-        //read content
-        for( ; ; now++)
-        {
-            if(now + 1 >= code.size())return 0;
-            if(code[now] == '<')
-            {
-                if(code[now+1] == '/')
-                {
-                    now += 2;
-                    for(;;now++,start++)
-                    {
-                        if(now >= code.size())return 0;
-                        if(code[now] != code[start])return 0;
-                        if(code[now] == '>')return 1;
-                    }
-                }
-                else if(code[now+1] == '!')
-                {
-                    if(!cdata(code))return 0;
-                }
-                else if(!tag(code))return 0;
-            }
-        }
-        return 0;
-    }
 public:
     bool isValid(string code) {
-        now = 0;
-        return tag(code) && now == code.size()-1;
+        int i = 0;
+        return validTag(code, i) && i == code.size();
+    }
+private:
+    bool validTag(string s, int& i) {
+        int j = i;
+        string tag = parseTagName(s, j);
+        if (tag.empty()) return false;
+        if (!validContent(s, j)) return false;
+        int k = j + tag.size() + 2;
+        if (k >= s.size() || s.substr(j, k + 1 - j) != "</" + tag + ">") return false;
+        i = k + 1;
+        return true;
+    }
+    string parseTagName(string s, int& i) {
+        if (s[i] != '<') return "";
+        int j = s.find('>', i);
+        if (j == string::npos || j - 1 - i < 1 || 9 < j - 1 - i) return "";
+        string tag = s.substr(i + 1, j - 1 - i);
+        for (char ch : tag) {
+            if (ch < 'A' || 'Z' < ch) return "";
+        }
+        i = j + 1;
+        return tag;
+    }
+    bool validContent(string s, int& i) {
+        int j = i;
+        while (j < s.size()) {
+            if (!validText(s, j) && !validCData(s, j) && !validTag(s, j)) break;
+        }
+        i = j;
+        return true;
+    }
+    bool validText(string s, int& i) {
+        int j = i;
+        while (i < s.size() && s[i] != '<') { i++; }
+        return i != j;
+    }
+    bool validCData(string s, int& i) {
+        if (s.find("<![CDATA[", i) != i) return false;
+        int j = s.find("]]>", i);
+        if (j == string::npos) return false;
+        i = j + 3;
+        return true;
     }
 };
