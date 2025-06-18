7b7c1281bdf48d044ae4fd0fa82728a41fad3d2c
buffer <iamrealhayden@gmail.com>
2025-06-18 10:31:22 +0000

    chore: sync automated leetcode submissions with Runtime - 0 ms (100.00%), Memory - 8.7 MB (86.56%)

--- 1058-lexicographically-smallest-equivalent-string/solution.cpp
+++ 1058-lexicographically-smallest-equivalent-string/solution.cpp
@@ -1,34 +1,30 @@
-#include <string>
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
-    string smallestEquivalentString(string s1, string s2, string baseStr) {
-        vector<int> parent(26);
-        iota(parent.begin(), parent.end(), 0);
-        
-        function<int(int)> find = [&](int x) {
-            return parent[x] == x ? x : parent[x] = find(parent[x]);
-        };
-        
-        for (int i = 0; i < s1.size(); ++i) {
-            int a = find(s1[i] - 'a'), b = find(s2[i] - 'a');
-            if (a != b) parent[max(a, b)] = min(a, b);
+    int find(int node, vector<int>&parent)
+    {
+        if(node != parent[node])
+        {
+            parent[node] = find(parent[node], parent);
         }
-        
-        string result;
-        result.reserve(baseStr.size());
-        for (char c : baseStr) {
-            result += char('a' + find(c - 'a'));
-        }
-        
-        return result;
+        return parent[node];
     }
-};
+    string smallestEquivalentString(string s1, string s2, string word) {
+        vector<int>parent(26);
+        for(int i=0;i<26;i++)parent[i] =i;
+        for(int i=0;i<s1.size();i++)
+        {
+            int u = find(s1[i]-'a', parent);
+            int v = find(s2[i]-'a', parent);
+            if(u!=v) parent[max(u, v)] = min(u, v);
 
-static const auto _ = []() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    return 0;
-}();
+        }
+        string res ="";
+        for(char ch:word)
+        {
+            res+=char(find(ch-'a', parent)+ 'a');
+        }
+        return res;
+
+    }
+       
+};
