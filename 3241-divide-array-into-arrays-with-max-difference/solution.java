49116ca8cf63a97b60480e1ef1d3bbea2dd01fb7
buffer <iamrealhayden@gmail.com>
2025-06-18 10:23:06 +0000

    chore: sync automated leetcode submissions with Runtime - 10 ms (98.11%), Memory - 56.6 MB (99.83%)

--- 3241-divide-array-into-arrays-with-max-difference/solution.java
+++ 3241-divide-array-into-arrays-with-max-difference/solution.java
@@ -1,18 +1,25 @@
-import java.util.*;
-
 class Solution {
     public int[][] divideArray(int[] nums, int k) {
-        Arrays.sort(nums);
-        int n = nums.length;
-        int[][] result = new int[n / 3][3];
-        
-        for (int i = 0; i < n; i += 3) {
-            if (nums[i + 2] - nums[i] > k) {
-                return new int[0][0];
+        int max = Integer.MIN_VALUE;
+        for(int i: nums)
+            max = Math.max(i, max);
+        int[] count = new int[max+1];
+        for(int i:nums)
+            count[i]++;
+        int[][] res = new int[nums.length/3][3];
+        int row=0, col=0;
+        for(int i=0;i<max+1;i++){
+            if(count[i]>0){
+                while(count[i]>0){
+                    res[row][col++] = i;
+                    if(col==3){
+                        if(Math.abs(res[row][0]-res[row][2])>k) return new int[0][0];
+                        row++;col=0;
+                    }
+                    count[i]--;
+                }
             }
-            result[i / 3] = new int[]{nums[i], nums[i + 1], nums[i + 2]};
-        }
-        
-        return result;
+        }  
+        return res;
     }
 }
