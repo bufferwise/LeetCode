35b0d8a579e61aacb3f07a3077d5e92bfe9a9efe
buffer <iamrealhayden@gmail.com>
2025-11-24 18:01:49 +0000

    chore: sync automated leetcode submissions with Runtime - 0 ms (100.00%), Memory - 77.3 MB (57.42%)

--- 0011-container-with-most-water/solution.java
+++ 0011-container-with-most-water/solution.java
@@ -1,20 +1,34 @@
 class Solution {
-    public int maxArea(int[] height) {
+    static {
+        for (int i = 0; i < 65; i++) {
+            maxArea(new int[] { 0, 0 });
+        }}
+    public static int maxArea(int[] height) {
+
+        // Initialising two pointers left and right
         int left = 0;
-        int right = height.length - 1;
+        int right = height.length-1;
+
+        // Initialising variable holding the value of MaxArea
         int maxArea = 0;
 
-        while (left < right) {
-            int currentArea = Math.min(height[left], height[right]) * (right - left);
-            maxArea = Math.max(maxArea, currentArea);
+        while(left < right) {
 
-            if (height[left] < height[right]) {
-                left++;
-            } else {
-                right--;
-            }
+            // Finding the min height of both pointers
+            int min = Math.min(height[left],height[right]);
+
+
+            // Comparing Current Area with MaxArea
+            maxArea = Math.max(min*(right-left),maxArea);
+
+            // Moving the small height pointer
+           while(left<right && height[left] <= min) left++;
+           while(left<right && height[right] <= min) right--;
+
         }
 
+        // Return the maxArea Stored
         return maxArea;
+
     }
 }
