debb62c11ee596f17843c7f2b8154f4db07a0b78
buffer <iamrealhayden@gmail.com>
2025-11-24 17:59:32 +0000

    chore: sync automated leetcode submissions with Runtime - 1 ms (44.42%), Memory - 63.1 MB (12.13%)

--- 0011-container-with-most-water/solution.cpp
+++ 0011-container-with-most-water/solution.cpp
@@ -1,42 +1,21 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int n = height.size();
-        int l = 0, r = n - 1;
-        int maxVol = INT_MIN;
+        int left = 0;
+        int right = height.size() - 1;
+        int maxArea = 0;
 
-        while(l < r) {
-            maxVol = max(maxVol, (r - l) * min(height[l], height[r]));
-            // We can do THREE things here
-            // 1st:
-            // if (height[l] >= height[r]) r--;
-            // else l++;
-            // 2nd:
-            // if (height[l] < height[r]) l++;
-            // else r--;
-            // 3rd:
-            if (height[l] <= height[r]) l++;
-            else r--;
+        while (left < right) {
+            int currentArea = min(height[left], height[right]) * (right - left);
+            maxArea = max(maxArea, currentArea);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
         }
 
-        return maxVol;
-
-
-
-
-        // int maxArea = 0;
-        // int left = 0;
-        // int right = height.size() - 1;
-
-        // while (left < right) {
-        //     maxArea = max(maxArea, (right - left) * min(height[left], height[right]));
-        //     if (height[left] < height[right]) {
-        //         left++;
-        //     } 
-        //     else {
-        //         right--;
-        //     }
-        // }
-        // return maxArea;        
+        return maxArea;
     }
 };
