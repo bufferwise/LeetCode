573fca8dfced5f43fb1f7844e00acc5680343640
buffer <iamrealhayden@gmail.com>
2025-12-20 10:22:28 +0000

    chore: sync automated leetcode submissions with Runtime - 6 ms (21.51%), Memory - 10.3 MB (99.26%)

--- 0981-delete-columns-to-make-sorted/solution.c
+++ 0981-delete-columns-to-make-sorted/solution.c
@@ -1,24 +1,17 @@
-#include <string.h>
-
 int minDeletionSize(char** strs, int strsSize) {
-    if (strsSize <= 1) {
-        return 0;
-    }
+    int deleteColumns = 0;
 
-    int deleteCount = 0;
-    int colLength = strlen(strs[0]);
-
-    // Iterate through each column
-    for (int j = 0; j < colLength; j++) {
-        // Check rows in the current column
-        for (int i = 0; i < strsSize - 1; i++) {
-            // If the current character is greater than the next, it's not sorted
-            if (strs[i][j] > strs[i+1][j]) {
-                deleteCount++;
-                break; // No need to check the rest of this column
+        for (int i = 0; i < strlen(strs[0]); i++) 
+        {
+            for (int j = 1; j < strsSize; j++) 
+            {
+                if (strs[j][i] < strs[j - 1][i]) 
+                {
+                    deleteColumns++;
+                    break;
+                }
             }
         }
-    }
 
-    return deleteCount;
+        return deleteColumns;
 }
