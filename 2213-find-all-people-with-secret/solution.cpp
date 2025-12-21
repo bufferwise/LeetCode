85e295a9fa6ba01140528212e5550c499497c469
buffer <iamrealhayden@gmail.com>
2025-12-21 14:48:34 +0000

    chore: sync automated leetcode submissions with Runtime - 246 ms (38.80%), Memory - 327.4 MB (26.60%)

--- 2213-find-all-people-with-secret/solution.cpp
+++ 2213-find-all-people-with-secret/solution.cpp
@@ -1,68 +1,80 @@
 #include <vector>
 #include <algorithm>
+#include <queue>
 
 using namespace std;
 
 class Solution {
 public:
-    
-    int find(vector<int>& parent, int i) {
-        if (parent[i] == i) return i;
-        return parent[i] = find(parent, parent[i]);
-    }
-
-    
-    void unite(vector<int>& parent, int i, int j) {
-        int root_i = find(parent, i);
-        int root_j = find(parent, j);
-        if (root_i != root_j) parent[root_i] = root_j;
-    }
-
     vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int firstPerson) {
         
         sort(meetings.begin(), meetings.end(), [](const vector<int>& a, const vector<int>& b) {
             return a[2] < b[2];
         });
 
-        vector<int> parent(n);
-        for (int i = 0; i < n; i++) parent[i] = i;
-
         
-        unite(parent, 0, firstPerson);
+        vector<bool> knows(n, false);
+        knows[0] = true;
+        knows[firstPerson] = true;
 
         int m = meetings.size();
+        
+        vector<pair<int, int>> adj[n]; 
+        
+        
         int i = 0;
         while (i < m) {
             int currentTime = meetings[i][2];
             int j = i;
             
+            static vector<int> peopleAtTime;
+            peopleAtTime.clear();
             
-            vector<int> pool;
+            
             while (j < m && meetings[j][2] == currentTime) {
                 int u = meetings[j][0];
                 int v = meetings[j][1];
-                unite(parent, u, v);
-                pool.push_back(u);
-                pool.push_back(v);
+                
+                
+                if (adj[u].empty()) peopleAtTime.push_back(u);
+                if (adj[v].empty()) peopleAtTime.push_back(v);
+                
+                adj[u].push_back({v, currentTime});
+                adj[v].push_back({u, currentTime});
                 j++;
             }
 
-            
-            int root0 = find(parent, 0);
-            for (int p : pool) {
-                if (find(parent, p) != root0) {
-                    parent[p] = p;
+            queue<int> q;
+            for (int p : peopleAtTime) {
+                if (knows[p]) {
+                    q.push(p);
                 }
             }
+
+            while (!q.empty()) {
+                int curr = q.front();
+                q.pop();
+                
+                for (auto& edge : adj[curr]) {
+                    int neighbor = edge.first;
+                    if (!knows[neighbor]) {
+                        knows[neighbor] = true;
+                        q.push(neighbor);
+                    }
+                }
+            }
+
+
+            for (int p : peopleAtTime) {
+                adj[p].clear();
+            }
+            
             i = j;
         }
 
         vector<int> result;
-        int root0 = find(parent, 0);
         for (int k = 0; k < n; k++) {
-            if (find(parent, k) == root0) {
-                result.push_back(k);
-            }
+            if (knows[k]) result.push_back(k);
         }
         return result;
     }
