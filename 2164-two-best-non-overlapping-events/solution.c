5bff854a8670a727cb1ea8499fe4da3ff4dd832a
buffer <iamrealhayden@gmail.com>
2025-12-23 05:36:53 +0000

    chore: sync automated leetcode submissions with Runtime - 47 ms (50.00%), Memory - 46.4 MB (0.00%)

--- 2164-two-best-non-overlapping-events/solution.c
+++ 2164-two-best-non-overlapping-events/solution.c
@@ -1,85 +1,46 @@
-#pragma GCC optimize("Ofast,unroll-loops")
-#pragma GCC target("avx2,bmi2,mmx,sse2,sse3,sse4")
+#include <stdio.h>
+#include <stdlib.h>
 
-#define START 0
-#define END 1
-#define VALUE 2
-
-
-int compare_func(const void* a, const void* b) {
-    int arg1 = (*(const int**)a)[START];
-    int arg2 = (*(const int**)b)[START];
-
-    if (arg1 < arg2) {
-        return -1;
-    } 
-    if (arg1 > arg2) {
-        return 1;
-    }
-
-    return 0;
-}
-
-inline void binarySearch(int** events, int left, int right, int end_val, int* res) {
-    if (left > right) {
-        return ;
-    }
-
-    int mid = (left + right) >> 1;
-
-    if (events[mid][START] > end_val) {
-        if (mid < *res) {
-            *res = mid;
-        }
-
-        binarySearch(events, left, mid - 1, end_val, res);
-    } else {
-        binarySearch(events, mid + 1, right, end_val, res);
-    }
+int compare(const void* a, const void* b) {
+    int* eventA = *(int**)a;
+    int* eventB = *(int**)b;
+    return eventA[0] - eventB[0];
 }
 
 int maxTwoEvents(int** events, int eventsSize, int* eventsColSize) {
 
-    qsort(events, eventsSize, sizeof(int*), compare_func);
+    qsort(events, eventsSize, sizeof(int*), compare);
 
-    int suffix_max[eventsSize];
-    memset(suffix_max, 0, eventsSize * sizeof(int));
-
-    suffix_max[eventsSize - 1] = events[eventsSize - 1][VALUE];
+    int* suffixMax = (int*)malloc(sizeof(int) * eventsSize);
+    suffixMax[eventsSize - 1] = events[eventsSize - 1][2];
     for (int i = eventsSize - 2; i >= 0; i--) {
-        if (events[i][VALUE] > suffix_max[i + 1]) {
-            suffix_max[i] = events[i][VALUE];
-        } else {
-            suffix_max[i] = suffix_max[i + 1];
-        }
+        suffixMax[i] = (events[i][2] > suffixMax[i + 1]) ? events[i][2] : suffixMax[i + 1];
     }
 
-    int next_event = 0;
-    int cur_event = 0;
-
-    int ret = INT_MIN;
+    int maxSum = 0;
 
     for (int i = 0; i < eventsSize; i++) {
-        int res_next = INT_MAX;
+        if (events[i][2] > maxSum) maxSum = events[i][2];
+        int target = events[i][1] + 1;
+        int left = i + 1, right = eventsSize - 1;
+        int bestFutureIdx = -1;
         
-        binarySearch(events, i, eventsSize - 1, events[i][END], &res_next);
-
-        int cur_val;
-
-        if (res_next < INT_MAX) {
-            cur_val = events[i][VALUE] + suffix_max[res_next];   
-        } else {
-            cur_val = events[i][VALUE];
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (events[mid][0] >= target) {
+                bestFutureIdx = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
         }
 
-        ret = (ret > cur_val)? ret : cur_val;
+        if (bestFutureIdx != -1) {
+            int currentSum = events[i][2] + suffixMax[bestFutureIdx];
+            if (currentSum > maxSum) maxSum = currentSum;
+        }
     }
 
-    /*
-    for (int i = 0; i < eventsSize; i++) {
-        printf("%d ", events[i][START]);
-    }
-    */
-
-    return ret;
+    free(suffixMax);
+    return maxSum;
 }
