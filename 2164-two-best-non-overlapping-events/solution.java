1a4d30374a30dbe80e9cf46d0367bb3e86069282
buffer <iamrealhayden@gmail.com>
2025-12-23 16:59:06 +0000

    chore: sync automated leetcode submissions with Runtime - 133 ms (8.04%), Memory - 191.1 MB (7.54%)

--- 2164-two-best-non-overlapping-events/solution.java
+++ 2164-two-best-non-overlapping-events/solution.java
@@ -1,23 +1,26 @@
 class Solution {
-    public int maxTwoEvents(int[][] events) {
-        
-Arrays.sort(events, (a, b) -> a[0] - b[0]);
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+  public int maxTwoEvents(int[][] events) {
+    record Event(int time, int value, int isStart) {}
+    int ans = 0;
+    int maxValue = 0;
+    Event[] evts = new Event[events.length * 2];
 
-        int maxSingle = 0, result = 0;
-
-        for (int[] event : events) {
-            int start = event[0], end = event[1], value = event[2];
-
-            while (!pq.isEmpty() && pq.peek()[1] < start) {
-                maxSingle = Math.max(maxSingle, pq.poll()[2]);
-            }
-
-            result = Math.max(result, maxSingle + value);
-            pq.offer(event);
-        }
-
-        return result;
-
+    for (int i = 0; i < events.length; ++i) {
+      final int start = events[i][0];
+      final int end = events[i][1];
+      final int value = events[i][2];
+      evts[i * 2] = new Event(start, value, 1);
+      evts[i * 2 + 1] = new Event(end + 1, value, 0);
     }
+
+    Arrays.sort(evts, Comparator.comparingInt(Event::time).thenComparingInt(Event::isStart));
+
+    for (Event evt : evts)
+      if (evt.isStart == 1)
+        ans = Math.max(ans, evt.value + maxValue);
+      else
+        maxValue = Math.max(maxValue, evt.value);
+
+    return ans;
+  }
 }
