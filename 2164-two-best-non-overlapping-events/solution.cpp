f2803a487c5d29ed50cbd8144191f6fc760b0f1a
buffer <iamrealhayden@gmail.com>
2025-12-23 16:55:56 +0000

    chore: sync automated leetcode submissions with Runtime - 84 ms (54.10%), Memory - 156.2 MB (100.00%)

--- 2164-two-best-non-overlapping-events/solution.cpp
+++ 2164-two-best-non-overlapping-events/solution.cpp
@@ -1,62 +1,50 @@
-#include <vector>
-#include <algorithm>
-
-using namespace std;
-
-struct Event {
-    int start, end, val;
-};
-
 class Solution {
+    private:
+    int n;
+    int findUpperBound(vector<vector<int>>& ev, int k){
+
+        int l = -1, r= n;
+
+        while(r-l>1){
+            int mid = l+(r-l)/2;
+            if(ev[mid][0]>k) r= mid;
+            else l = mid;
+        }
+
+        return r;
+    }
 public:
-    int maxTwoEvents(vector<vector<int>>& events) {
+    
 
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
+    int maxTwoEvents(vector<vector<int>>& ev) {
 
-        int n = events.size();
+        n = ev.size();
 
-        vector<Event> evs(n);
-        for (int i = 0; i < n; ++i) {
-            evs[i] = {events[i][0], events[i][1], events[i][2]};
+        sort(ev.begin(),ev.end());
+
+        int maxi = 0;
+
+        for(int i=n-1;i>=0;i--){
+            maxi = max(maxi,ev[i][2]);
+            ev[i].push_back(maxi);
         }
 
-        sort(evs.begin(), evs.end(), [](const Event& a, const Event& b) {
-            return a.start < b.start;
-        });
+        maxi = 0;
 
-        vector<int> suffixMax(n + 1, 0);
-        suffixMax[n - 1] = evs[n - 1].val;
-        for (int i = n - 2; i >= 0; --i) {
-            suffixMax[i] = max(evs[i].val, suffixMax[i + 1]);
+        for(int i=0;i<n;i++){
+            int e = ev[i][1];
+            int cv = ev[i][2];
+
+            int ub = findUpperBound(ev,e);
+
+            if(ub!=n) cv+=ev[ub][3];
+
+            maxi = max(maxi, cv);
+
+
         }
 
-        int ans = 0;
-        for (int i = 0; i < n; ++i) {
-
-            int currentVal = evs[i].val;
-
-            int target = evs[i].end;
-            int left = i + 1, right = n - 1;
-            int nextBestIdx = -1;
-
-            while (left <= right) {
-                int mid = left + (right - left) / 2;
-                if (evs[mid].start > target) {
-                    nextBestIdx = mid;
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
-            }
-
-            if (nextBestIdx != -1) {
-                currentVal += suffixMax[nextBestIdx];
-            }
-            
-            if (currentVal > ans) ans = currentVal;
-        }
-
-        return ans;
+        return maxi;
+        
     }
 };
