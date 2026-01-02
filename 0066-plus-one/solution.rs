1c001a651db8bffa9c15aaaa10a53ceb73a1b123
buffer <iamrealhayden@gmail.com>
2026-01-02 08:58:50 +0000

    chore: sync automated leetcode submissions with Runtime - 0 ms (100.00%), Memory - 2.1 MB (93.87%)

---
+++ 0066-plus-one/solution.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn plus_one(mut digits: Vec<i32>) -> Vec<i32> {
+        let n = digits.len();
+        
+        for i in (0..n).rev() {
+            if digits[i] < 9 {
+                digits[i] += 1;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        digits[0] = 1;
+        digits.push(0);
+        digits
+    }
+}
